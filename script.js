c0efb1a9aec405da97e21d3338193ee3071b6636
MadiBentley <mb.madisonbentley@gmail.com>
2017-02-13 22:45:35 -0800

    added inventory section

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 var buildBoard = function(){
   $( document ).ready(function() {
-    $("#board").css("width", "800").css("font-size", "0");
-    for (var x = 0; x< 800; x++){
+    $("#board").css("width", "800").css("display","inline-block").css("font-size", "0");
+    for (var x = 10; x < 40; x++){
+      for (var y = 10; y < 40; y++){
         $("#board").append("<div class='block' style='display:inline-block; margin:0px; padding:0px; width:25px; height:25px; background-color:black'></div>");
+      }
     }
-    $(".block").hover(function(){
-    $(this).css("background-color", "yellow");
-    }, function(){
-    $(this).css("background-color", "pink");
-});
   });
 }
+var buildInventory = function(){
+  $(document).ready(function(){
+    $("#gameInfo").css("width", "300px").css("height", "200px").css("display", "inline-block");
+  });
+}
+buildInventory();
 buildBoard();
