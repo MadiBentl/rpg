ef7dfa9c33c032bf3dbf59457cace998522102af
MadiBentley <mb.madisonbentley@gmail.com>
2017-02-14 22:32:11 -0800

    made initial square grey

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var buildBoard = function(){
     $("#board").css("width", "750").css("display","inline-block").css("font-size", "0");
     for (var x = 10; x < 40; x++){
       for (var y = 10; y < 40; y++){
-        $("#board").append("<div id='" + x + y + "' class='tile' style='display:inline-block; margin:0px; padding:0px; width:25px; height:25px; background-color:black'></div>");
+        $("#board").append("<div id='t" + x + y + "' class='tile' style='display:inline-block; margin:0px; padding:0px; width:25px; height:25px; background-color:black'></div>");
       }
     }
   });
@@ -13,5 +13,12 @@ var buildInventory = function(){
     $("#gameInfo").css("width", "300px").css("height", "200px").css("display", "inline-block");
   });
 }
+var assignSpriteStart = function(){
+  $(document).ready(function(){
+    $("#t1514").css("background-color","grey");
+    console.log("executed");
+  });
+}
 //buildInventory();
 buildBoard();
+assignSpriteStart();
