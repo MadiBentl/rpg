0f85b6576e0199f38f954ee0682c200d9410329d
MadiBentley <mb.madisonbentley@gmail.com>
2017-02-14 22:50:08 -0800

    Added char to div

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var buildBoard = function(){
     $("#board").css("width", "750").css("display","inline-block").css("font-size", "0");
     for (var x = 10; x < 40; x++){
       for (var y = 10; y < 40; y++){
-        $("#board").append("<div id='t" + x + y + "' class='tile' style='display:inline-block; margin:0px; padding:0px; width:25px; height:25px; background-color:black'></div>");
+        $("#board").append("<div id='t" + x + y + "' class='tile' style='display:inline-block; overflow:hidden; font-size:large; margin:0px; padding:0px; width:25px; height:25px; background-color:black'></div>");
       }
     }
   });
@@ -15,10 +15,11 @@ var buildInventory = function(){
 }
 var assignSpriteStart = function(){
   $(document).ready(function(){
-    $("#t1514").css("background-color","grey");
+    $("#t1514").css("background-color","grey").text("@");
     console.log("executed");
   });
 }
+
 //buildInventory();
 buildBoard();
 assignSpriteStart();
