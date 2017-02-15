f26be5a7dd4e51435fc78eb8671e19e9a54b541e
MadiBentley <mb.madisonbentley@gmail.com>
2017-02-14 22:22:30 -0800

    Add tile IDs

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var buildBoard = function(){
   $( document ).ready(function() {
-    $("#board").css("width", "800").css("display","inline-block").css("font-size", "0");
+    $("#board").css("width", "750").css("display","inline-block").css("font-size", "0");
     for (var x = 10; x < 40; x++){
       for (var y = 10; y < 40; y++){
-        $("#board").append("<div class='block' style='display:inline-block; margin:0px; padding:0px; width:25px; height:25px; background-color:black'></div>");
+        $("#board").append("<div id='" + x + y + "' class='block' style='display:inline-block; margin:0px; padding:0px; width:25px; height:25px; background-color:black'></div>");
       }
     }
   });
@@ -13,5 +13,5 @@ var buildInventory = function(){
     $("#gameInfo").css("width", "300px").css("height", "200px").css("display", "inline-block");
   });
 }
-buildInventory();
+//buildInventory();
 buildBoard();
