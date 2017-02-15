78e2f262e9774e78f0f6d4891532c7657f932e98
MadiBentley <mb.madisonbentley@gmail.com>
2017-02-14 22:23:07 -0800

    Rename blocks as tiles

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var buildBoard = function(){
     $("#board").css("width", "750").css("display","inline-block").css("font-size", "0");
     for (var x = 10; x < 40; x++){
       for (var y = 10; y < 40; y++){
-        $("#board").append("<div id='" + x + y + "' class='block' style='display:inline-block; margin:0px; padding:0px; width:25px; height:25px; background-color:black'></div>");
+        $("#board").append("<div id='" + x + y + "' class='tile' style='display:inline-block; margin:0px; padding:0px; width:25px; height:25px; background-color:black'></div>");
       }
     }
   });
