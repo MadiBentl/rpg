b0e65083065b3b0ea7a747f9a5f51173fad7007d
MadiBentley <mb.madisonbentley@gmail.com>
2017-02-15 21:06:52 -0800

    Add sass

--- script.js
+++ script.js
@@ -20,6 +20,26 @@ var assignSpriteStart = function(){
   });
 }
 
+  $(document).keydown(function(e) {
+    switch(e.which) {
+        case 37: // left
+        break;
+
+        case 38: // up
+        break;
+
+        case 39: // right
+        console.log("right");
+        break;
+
+        case 40: // down
+        break;
+
+        default: return; // exit this handler for other keys
+    }
+    e.preventDefault(); // prevent the default action (scroll / move caret)
+});
+
 //buildInventory();
 buildBoard();
 assignSpriteStart();
