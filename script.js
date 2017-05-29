0bfb827f469c886100b92f8e89f2e87fba5e1798
MadiBentley <mb.madisonbentley@gmail.com>
2017-05-28 21:38:31 -0700

    changed .text to span

--- script.js
+++ script.js
@@ -1346,7 +1346,7 @@ var inventory = {
     var x = 1;
     for (var prop in my_inv){
       if (my_inv[prop]["qty"] > 0){
-        $("#i" + x).text(my_inv[prop]["symbol"]).append("<p style='font-size: .5em'>" + my_inv[prop]["qty"]+ "</p>");
+        $("#i" + x).html("<span style='disply:inline-block'>" + my_inv[prop]["symbol"] + "</span><span style='font-size: .6em; line-height: 0px; disply:inline-block'>" + my_inv[prop]["qty"]+ "</span>");
         x++;
       }
     }
